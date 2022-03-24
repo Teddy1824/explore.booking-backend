eb7362fe8e9b1971148b7baf648055bb5c8f8829
Bongani <bearnomangola7@gmail.com>
2022-03-24 13:11:58 +0200

    saved to heroku and changed my post router in places.routes

--- app/routes/places.routes.js
+++ app/routes/places.routes.js
@@ -182,10 +182,8 @@ app.get("/", async (req, res) => {
     }
 });
 
-app.get("/:id", (req, res) => {
-    const place = places.find(place => place.id === req.params.id);
-    if (!place) res.status(404).send('Place not found... :(')
-    return res.send(place)
+app.get("/:id", getPlaces, (req, res) => {
+    res.send(req.places)
 });
 
 app.post("/", async (req, res) => {
